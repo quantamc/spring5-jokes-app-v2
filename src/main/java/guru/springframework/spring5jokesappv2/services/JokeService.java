9eb610b45e1063c0825aac6694d3b4e531e61113
Thabo Nonofo Malete <thabo.malete@fnbbotswana.co.bw>
2022-05-12 01:00:26 +0200

    Creating Spring Service layer - tm

---
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokeService.java
@@ -0,0 +1,10 @@
+package guru.springframework.spring5jokesappv2.services;
+
+/***
+ * Created by tm on 12/05/2022
+ */
+
+public interface JokeService {
+
+    String getJoke();
+}
---
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokeServiceImpl.java
@@ -0,0 +1,22 @@
+package guru.springframework.spring5jokesappv2.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by tm on 12/05/22
+ */
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
